10c45d22167715ac8834a0d12dfe804ced7fb3d5
pavanrakurty <saipavanrakurty@gmail.com>
2026-06-17 00:01:02 +0530

    array loops

--- scripts/11_todo_list.js
+++ scripts/11_todo_list.js
@@ -1,4 +1,6 @@
-let todo = JSON.parse(localStorage.getItem('todo-item')) || [];
+let todo = [];
+
+renderToDoList();
 
 function UpdateToDoList(){
   const item = document.querySelector('.todo-value').value;
@@ -6,10 +8,27 @@ function UpdateToDoList(){
   localStorage.setItem('todo-item', JSON.stringify(todo));
   console.log(todo);
   document.querySelector('.todo-value').value = '';
+  renderToDoList();
 }
 
 function UpdateToDoListKeyDown(event){
   if (event.key==='Enter'){
     UpdateToDoList();
   }
+}
+
+function renderToDoList(){
+  let toDOList = '';
+  for (let i=0; i<todo.length; i++){
+    toDOList += `<p>
+                  ${todo[i]}
+                <button onClick=
+                  "
+                  todo.splice(${i},1);
+                  renderToDoList();
+                  "
+                >Delete</button>  
+                </p>`;
+  }
+  document.querySelector('.todo-list').innerHTML = toDOList;
 }
