4a312c0bd5a0469e80599c348bfe0c62d407546c
pavanrakurty <saipavanrakurty@gmail.com>
2026-06-22 22:32:31 +0530

    Generate HTML for order summary

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,12 +1,22 @@
-export const cart = [];
+export const cart = [
+
+  {
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+  },
+  {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 3
+  }
+];
 
 export function addToCart(productId){
 
   let matchingItem;
 
-  cart.forEach((item)=>{
-    if(item.productId===productId){
-      matchingItem = item;
+  cart.forEach((cartItem)=>{
+    if(cartItem.productId===productId){
+      matchingItem = cartItem;
     }
   });
 
