0769f4746840642b80c0975dcf4b3fb06492ac6d
pavanrakurty <saipavanrakurty@gmail.com>
2026-06-23 18:27:24 +0530

    making delete item from cart button interactive

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem) => {
   });
 
   checkoutSummaryHTML += `
-<div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -95,11 +95,12 @@ cart.forEach((cartItem) => {
 
 document.querySelector(".order-summary").innerHTML = checkoutSummaryHTML;
 
-
 document.querySelectorAll(".js-delete-cart-item").forEach((deleteLink) => {
   deleteLink.addEventListener("click",()=>{
     const productId = deleteLink.dataset.productId;
     removeCartItem(productId);
-    console.log(cart);
+    
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   });
 });
