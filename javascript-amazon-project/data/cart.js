fce2a476fc38d145ce6a410fdd0bde392802cc06
pavanrakurty <saipavanrakurty@gmail.com>
2026-06-23 10:22:32 +0530

    Delete functionality added to delete items in cart

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
 
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -29,5 +29,18 @@ export function addToCart(productId){
     quantity:1
   });
   };
+
+};
+
+export function removeCartItem(productId){
+  
+  const newCart = [];
+  cart.forEach((cartItem)=>{
+    if(cartItem.productId !== productId){
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 
 };
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart,removeCartItem} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -38,7 +38,7 @@ cart.forEach((cartItem) => {
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-cart-item" data-product-id=${matchingProduct.id}>
             Delete
           </span>
         </div>
@@ -94,3 +94,12 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector(".order-summary").innerHTML = checkoutSummaryHTML;
+
+
+document.querySelectorAll(".js-delete-cart-item").forEach((deleteLink) => {
+  deleteLink.addEventListener("click",()=>{
+    const productId = deleteLink.dataset.productId;
+    removeCartItem(productId);
+    console.log(cart);
+  });
+});
