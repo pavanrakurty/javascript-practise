1c92e50a4aff5a77135f9ad73ab91e275a7fa431
pavanrakurty <saipavanrakurty@gmail.com>
2026-06-28 19:33:40 +0530

    Use inheritance and polymorphism to product

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -23,7 +23,29 @@ class Product{
   getImageURL(){
     return `images/ratings/rating-${this.rating.stars*10}.png`;
   }
+
+  extraInfoHTML(){
+    return '';
+  }
 }
+
+class Clothing extends Product{
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML(){
+    return `
+      <a href = '${this.sizeChartLink}' target = '_blank'>
+        Size chart
+      </a>
+    `;
+  }
+}
+
 
 export const products = [
   {
@@ -685,6 +707,9 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
 
