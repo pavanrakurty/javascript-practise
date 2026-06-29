fd9250ffcd83d82246f025d2f92c6a507fccf9ac
pavanrakurty <saipavanrakurty@gmail.com>
2026-06-29 17:53:16 +0530

    Load products from backend

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,6 +1,11 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {paymentSummary} from './checkout/paymentSummary.js';
-import '../data/car.js'
+// import '../data/car.js';
+// import '../data/backend-practise.js'
+import {loadProducts} from '../data/products.js'
 
-renderOrderSummary();
-paymentSummary();
+
+loadProducts(()=>{
+  renderOrderSummary();
+  paymentSummary();
+});
